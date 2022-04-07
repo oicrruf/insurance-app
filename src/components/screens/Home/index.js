9f50c78bf4a1e0681fd49828a0163eff4df5d37c
rodval <35208967+rodval@users.noreply.github.com>
2022-04-06 22:59:06 -0600

    feat: install and configuration of react-native-responsive-screen
    create home component with react-native-responsive-screen

---
+++ src/components/screens/Home/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import {  
+    heightPercentageToDP as hp,
+    widthPercentageToDP as wp
+} from 'react-native-responsive-screen';
+import { Text } from '@rneui/themed';
+
+const Home = () =>{
+    return (
+        <View style={styles.container}>
+            <Text>Home</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: hp('100%'),
+        width: wp('100%')
+    }
+})
+
+export default Home;
